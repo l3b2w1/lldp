3270cec70fd5322dbdfd2cef90307b2ae10cf2fc
l3b2w1 <l3b2w1_linux@163.com>
2016-04-23 14:28:55 +0800

    change the free place

--- rx_sm.c
+++ rx_sm.c
@@ -275,8 +275,6 @@ int rxProcessFrame(struct lldp_port *lldp_port)
 			    have_dc_msap = 1;
 			else 
 			  have_dc_msap = 0;
-			  
-			free(msap_dctlv.value);
 		}
 
 		tlv_offset += sizeof(*tlv_hdr) + tlv_length;
@@ -285,7 +283,7 @@ int rxProcessFrame(struct lldp_port *lldp_port)
 
 		destroy_tlv(&tlv);
 
-
+		free(msap_dctlv.value);
 
 	} while (tlv_type != 0);
 
